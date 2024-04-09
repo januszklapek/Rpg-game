ec479d19aa7731997ff465bbe15096c646e2cc3e
januszklapek <145707974+januszklapek@users.noreply.github.com>
2024-04-09 23:45:10 +0200

    enemis.py

--- enemis.py
+++ enemis.py
@@ -1,7 +1,30 @@
 from characterandenemy import Character
 from characterandenemy import Enemy
 all_enemies = [
-    Enemy(name="Szkielet Orka", health=50, attack=15, defense=5, experience=25, loot=["Złoto", "Mikstura leczenia"]),
-    Enemy(name="Gromada szczórów ze wścieklizną", health=70, attack=10, defense=8, experience=30, loot=["Miecz", "Zbroja"]),
-    Enemy(name="Król Michał Korybut Wiśniowiecki ", health=100, attack=20, defense=15, experience=50, loot=["Legendarny miecz"]),
-    Enemy(name="Jedno oki Goblin zwny Joe", health=40, attack=12, defense=3, experience=20, loot=["Złoto"])]
+    Enemy(name="Szkielet Orka", 
+          health=50, 
+          attack=15, 
+          defense=5, 
+          experience=25, 
+          loot=["Złoto", "Mikstura leczenia"]),
+
+    Enemy(name="Gromada szczórów ze wścieklizną", 
+          health=70, 
+          attack=10, 
+          defense=8, 
+          experience=30, 
+          loot=["Miecz", "Zbroja"]),
+
+    Enemy(name="Król Michał Korybut Wiśniowiecki ", 
+          health=100, 
+          attack=20, 
+          defense=15, 
+          experience=50, 
+          loot=["Legendarny miec"]),
+
+    Enemy(name="Jedno oki Goblin zwny Joe", 
+          health=40, 
+          attack=12, 
+          defense=3, 
+          experience=20, 
+          loot=["Złoto"])]
